3879de229db4c368b94b679bf992008431042957
Cezar Augusto Crummenauer <cezar.augusto.web@gmail.com>
2019-01-06 16:43:08 -0200

    Criando o primeio model com o mongoose...

--- server.js
+++ server.js
@@ -5,8 +5,13 @@ const mongoose = require('mongoose');
 const app = express();
 
 // Iniciando o BD
+mongoose.connect(
+    'mongodb://localhost:27017/nodeapi', 
+    {useNewUrlParser: true}
+);
 
-mongoose.connect('mongodb://localhost:27017/nodeapi', {useNewUrlParser: true});
+// Registrar model na aplicação:
+require('./src/models/Product');
 
 // Criação da primeira rota
 app.get('/', (req, res) => {
